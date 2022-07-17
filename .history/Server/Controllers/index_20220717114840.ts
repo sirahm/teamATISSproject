d208f57ebb6091f4f5fb4448829d5aa2932caa2e
Ivan Tiutiukin <tyutyushkin71@gmail.com>
2022-07-17 12:00:55 -0400

    fix

--- .history/Server/Controllers/index_20220717114840.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { UserDisplayName } from '../Util';
-
-export function DisplayHomePage(req: express.Request, res: express.Response, next: express.NextFunction) 
-{
-  res.render('index', { title: 'Home', page: 'home', displayName: UserDisplayName(req) });
-}
-
