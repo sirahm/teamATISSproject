c3e2cdbe110e9eacb3c19432fb3b7da686ccd3b3
Tabriz123 <95578396+Tabriz123@users.noreply.github.com>
2022-07-17 17:58:42 -0500

    Updated Index.ts

--- Server/Controllers/index.ts
+++ Server/Controllers/index.ts
@@ -7,3 +7,17 @@ export function DisplayHomePage(req: express.Request, res: express.Response, nex
   res.render('index', { title: 'Home', page: 'home', displayName: UserDisplayName(req) });
 }
 
+export function DisplayCreateMCPage(req: express.Request, res: express.Response, next: express.NextFunction) 
+{
+  res.render('index', { title: 'Create Multiple Choice Question', page: 'createMC', displayName:  UserDisplayName(req) });
+}
+
+export function DisplayCreateTFPage(req: express.Request, res: express.Response, next: express.NextFunction) 
+{
+    res.render('index', { title: 'Create True/False Question', page: 'createTF', displayName: UserDisplayName(req)  });
+}
+
+export function DisplayCreateSAPage(req: express.Request, res: express.Response, next: express.NextFunction) 
+{
+    res.render('index', { title: 'Create Simple Answer Question', page: 'createSA', displayName: UserDisplayName(req)  });
+}
