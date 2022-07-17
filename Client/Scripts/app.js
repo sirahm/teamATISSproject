0343c805b5729d3f3efd992a9933b9e6959774c7
Tabriz123 <95578396+Tabriz123@users.noreply.github.com>
2022-07-17 18:33:03 -0500

    Updated app.js

--- Client/Scripts/app.js
+++ Client/Scripts/app.js
@@ -11,4 +11,17 @@
     }
     window.addEventListener("load", Start);
 })();
-//# sourceMappingURL=app.js.map
+
+function myFunction() {
+   
+    if (document.getElementById('MC').checked) {
+        location.href='/createMC';
+    }
+    else if (document.getElementById('TF').checked) {
+        location.href='/createTF';
+    }
+    else if (document.getElementById('SA').checked) {
+        location.href='/createSA';
+    }
+    
+}
